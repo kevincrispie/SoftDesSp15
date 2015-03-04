5f4eccfc1b104657067dc13c65c1855644d62c53
kevincrispie <kevincrispie@gmail.com>
2015-03-03 23:43:22 -0500

    changed some comments

--- toolbox/geocoding_apis/mbta_finder.py
+++ toolbox/geocoding_apis/mbta_finder.py
@@ -3,10 +3,7 @@ author: Kevin Crispie
 
 Geocoding and Web APIs Project Toolbox exercise
 
-Find the MBTA stops closest to a given location.
-
-Full instructions are at:
-https://sites.google.com/site/sd15spring/home/project-toolbox/geocoding-and-web-apis
+Finds the MBTA stops closest to a given location.
 """
 
 import urllib   # urlencode function
@@ -14,13 +11,10 @@ import urllib2  # urlopen function (better than urllib version)
 import json
 
 
-# Useful URLs (you need to add the appropriate parameters for your requests)
 GMAPS_BASE_URL = "https://maps.googleapis.com/maps/api/geocode/json"
 MBTA_BASE_URL = "http://realtime.mbta.com/developer/api/v2/stopsbylocation"
 MBTA_DEMO_API_KEY = "wX9NwuHnZU2ToO7GmGR9uw"
 
-
-# A little bit of scaffolding if you want to use it
 
 def encode_maps_url(place_name):
     base_url = "https://maps.googleapis.com/maps/api/geocode/"
@@ -33,7 +27,7 @@ def encode_maps_url(place_name):
 def get_json(url):
     """
     Given a properly formatted URL for a JSON web API request, return
-    a Python JSON object containing the response to that request.
+    a Python JSON object containing the response to that request
     """
 
     f = urllib2.urlopen(url)
